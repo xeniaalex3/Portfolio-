c734303c222a608b1bcae9b9f778a36085f742cb
xenia <xenia_onescu_3@hotmail.com>
2022-10-28 19:37:38 +0200

    build hero/about and header components

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,20 +1,25 @@
 import Head from 'next/head';
+import About from '../components/About';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Xenia Portfolio</title>
       </Head>
 
-      {/* Header */}
       <Header />
       {/* Hero */}
-
+      <section id='hero' className='snap-start'>
+       <Hero />
+      </section>
       {/* About */}
-
+<section id='about' className='snap-center'>
+  <About />
+</section>
       {/* Experience */}
 
       {/* Skills  */}
