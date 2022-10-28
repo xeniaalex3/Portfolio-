dbe90a47302c740b51cb32d454088ba9bc6d6024
xenia <xenia_onescu_3@hotmail.com>
2022-10-28 15:32:19 +0200

    install dependeces

---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import Header from '../components/Header';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Xenia Portfolio</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills  */}
+
+      {/* Projects  */}
+
+      {/* Contact me  */}
+
+    
+    </div>
+  )
+}
