61500b2f5b881b8aafba382050f7d88861465494
xenia <xenia_onescu_3@hotmail.com>
2022-11-03 16:21:43 +0100

    create experience and skills components

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,34 +1,37 @@
-import Head from 'next/head';
-import About from '../components/About';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import styles from '../styles/Home.module.css';
+import Head from 'next/head'
+import About from '../components/About'
+import Experience from '../components/Experience'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Skills from '../components/Skills'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Xenia Portfolio</title>
       </Head>
 
       <Header />
-      {/* Hero */}
-      <section id='hero' className='snap-start'>
-       <Hero />
+      <section id="hero" className="snap-start">
+        <Hero />
       </section>
-      {/* About */}
-     <section id='about' className='snap-center'>
-      <About />
+
+      <section id="about" className="snap-center">
+        <About />
+      </section>
+  
+      <section className='snap-center' id='experiences'>
+        <Experience />
+      </section>
+     
+     <section className='snap-center' id='skills'>
+       <Skills />
      </section>
-      {/* Experience */}
-
-      {/* Skills  */}
-
       {/* Projects  */}
 
       {/* Contact me  */}
-
-    
     </div>
   )
 }
