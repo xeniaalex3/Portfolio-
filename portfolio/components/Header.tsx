95ed95357a2a4a62beeb2ac24b24980d8a10f8a6
xenia <xenia_onescu_3@hotmail.com>
2022-11-06 14:37:12 +0100

    debugging componente

--- portfolio/components/Header.tsx
+++ portfolio/components/Header.tsx
@@ -1,69 +1,76 @@
+import Link from 'next/link'
 import React from 'react'
-import { SocialIcon } from 'react-social-icons';
-import { motion } from "framer-motion"
+import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
+
 type Props = {}
 
 function Header({}: Props) {
   return (
-    <header className='sticky top-0 p-5 flex items-start justify-between mx-auto max-w-7xl z-20 xl:items-center'>
-      <motion.div 
-      initial={{
-        x: -500,
-        opacity: 0,
-        scale: 0.5,
-      }}
-      animate={{
-        x: 0,
-        opacity: 1,
-        scale: 1,
-      }}
-      transition={{
-        duration: 1.4,
-      }}
-      className='flex flex-row items-center'>
-        {/* Social icons */}
-        <SocialIcon 
-        url="https://www.youtube.com/sonnysangha" 
-        fgColor='gray'
-        bgColor='transparent'
+    <header className="sticky top-0 p-5 flex items-start justify-between mx-auto max-w-7xl z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1.4
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://www.youtube.com/sonnysangha"
+          fgColor="gray"
+          bgColor="transparent"
         />
-         <SocialIcon 
-        url="https://www.linkedin.com/in/xenia-onescu" 
-        fgColor='gray'
-        bgColor='transparent'
+        <SocialIcon
+          url="https://www.linkedin.com/in/xenia-onescu"
+          fgColor="gray"
+          bgColor="transparent"
         />
-         <SocialIcon 
-        url="https://www.twitter.com/xenia-onescu" 
-        fgColor='gray'
-        bgColor='transparent'
+        <SocialIcon
+          url="https://www.twitter.com/xenia-onescu"
+          fgColor="gray"
+          bgColor="transparent"
         />
       </motion.div>
 
-      <motion.div 
-      initial={{
-        x:500,
-        opacity: 0,
-        scale: 0.5,
-      }}
-      animate={{
-        x:0,
-        opacity: 1,
-        scale: 1,
-      }}
-      transition={{
-        duration: 1.4,
-      }}
-      className='flex flex-row items-center text-gray-300 cursor-pointer'>
-      <SocialIcon 
-        className='cursor-pointer' 
-        network='email'
-        fgColor='gray'
-        bgColor='transparent'
-        />
-        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in Touch</p>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1.4
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <Link href="#contact">
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+        </Link>
+
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          <Link href="#contact">Get in Touch </Link>
+        </p>
       </motion.div>
-      
-      </header>
+    </header>
   )
 }
 
